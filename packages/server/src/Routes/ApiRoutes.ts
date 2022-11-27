1029036200ff7783b51051e72b97b84fc8499b70
Artur Poffo <arturpoffoc@hotmail.com>
2022-11-27 06:01:47 -0300

    Welcome Route in Server

--- packages/server/src/Routes/ApiRoutes.ts
+++ packages/server/src/Routes/ApiRoutes.ts
@@ -5,6 +5,7 @@ import UserController from "../controllers/UserController";
 
 const routes = Router()
 
+routes.get("/", (req, res) => res.status(200).json({ msg: "Productio Success" }))
 routes.get("/getUserData/:id", UserController.checkToken, UserController.getUserData)
 routes.post("/SignIn", UserController.SignIn)
 routes.post("/SignUp", UserController.SignUp)
