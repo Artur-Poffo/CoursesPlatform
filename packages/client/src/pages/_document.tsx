7bd0ec741fe3ffce3eaaec04d2cffb6fd53ffa13
Artur Poffo <arturpoffoc@hotmail.com>
2022-12-04 02:13:09 -0300

    Fix Bug

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -36,6 +36,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8' />
 
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap" rel="stylesheet" />
